79b7deff633d3c5088de2c3dececfff50064c610
trkha16 <tbkha1609@gmail.com>
2022-07-06 00:50:32 +0700

    update blog component

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -21,7 +21,39 @@ export default function Blog() {
                     </Grid>
                 </Grid>
                 <Grid container>
-                    <Grid item md={6}></Grid>
+                    <Grid item md={6} className={styles.mainArticle}>
+                        <div className={styles.banner}>
+                            <img
+                                src="https://templatemo.com/templates/templatemo_562_space_dynamic/assets/images/big-blog-thumb.jpg"
+                                alt="banner"
+                            />
+                        </div>
+                        <div className={styles.content}>
+                            <ul>
+                                <li>
+                                    <i class="fa-solid fa-calendar-days"></i>
+                                    24 Mar 2021
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-users"></i>
+                                    TemplateMo
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-folder"></i>
+                                    Branding
+                                </li>
+                            </ul>
+                            <h4>SEO Agency {"&"} Digital Marketing</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur and sed
+                                doer ket eismod tempor incididunt ut labore et
+                                dolore magna...
+                            </p>
+                        </div>
+                        <div className={styles.articleBtn}>
+                            <p>discover more</p>
+                        </div>
+                    </Grid>
                     <Grid item md={6} className={styles.listArticlesContainer}>
                         {articles.map((item) => (
                             <Grid container className={styles.listArticles}>
@@ -32,7 +64,7 @@ export default function Blog() {
                                 >
                                     <i class="fa-solid fa-calendar-days"></i>
                                     <span>{item.date}</span>
-                                    <h4>{item.title}</h4>
+                                    <h2>{item.title}</h2>
                                     <p>{item.content}</p>
                                 </Grid>
                                 <Grid
